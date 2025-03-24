750d27db2a894731c5a3dfd592fe4801123cbd8f
徐映白 <62323046+maplelost@users.noreply.github.com>
2025-03-24 16:47:40 +0800

    plugin-autostart

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -30,6 +30,7 @@ use tauri_plugin_shell::ShellExt;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_autostart::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_opener::init())
         .setup(|app| {
