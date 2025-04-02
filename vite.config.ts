edfa86ab40c9f031350f886745b123ed7787455a
徐映白 <62323046+maplelost@users.noreply.github.com>
2025-04-02 15:42:37 +0800

    @ alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
@@ -28,10 +34,10 @@ export default defineConfig(async () => ({
       // 3. tell vite to ignore watching `src-tauri`
       ignored: ["**/src-tauri/**"],
     },
-    resolve: {
-      alias: {
-        "@": "/src",
-      },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"), // 使用绝对路径
     },
   },
 }));
