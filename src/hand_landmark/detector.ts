72138f1fbf21618b4cbb7248a9dc3bc5e51e5e4c
徐映白 <62323046+maplelost@users.noreply.github.com>
2025-04-02 16:57:31 +0800

    使用本地资源

--- src/hand_landmark/detector.ts
+++ src/hand_landmark/detector.ts
@@ -23,13 +23,10 @@ export class Detector {
   private detector: HandLandmarker | null = null;
 
   async initialize() {
-    const vision = await FilesetResolver.forVisionTasks(
-      "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision/wasm"
-    );
+    const vision = await FilesetResolver.forVisionTasks("/mediapipe/wasm");
     this.detector = await HandLandmarker.createFromOptions(vision, {
       baseOptions: {
-        modelAssetPath:
-          "https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/1/hand_landmarker.task",
+        modelAssetPath: "/mediapipe/hand_landmarker.task",
         delegate: "GPU",
       },
       runningMode: "VIDEO",
