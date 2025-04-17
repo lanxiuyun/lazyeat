364224d4b880e9379b69bbc1c6c1f42d2985d367
徐映白 <62323046+maplelost@users.noreply.github.com>
2025-04-17 15:06:50 +0800

    Update SubWindow.vue

--- src/view/subWindow/SubWindow.vue
+++ src/view/subWindow/SubWindow.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
     <h1>子窗口</h1>
+    <span> camera: {{ app_store.cameras }}</span>
+    <span> camera-index: {{ app_store.config.selected_camera_id }}</span>
   </div>
 </template>
+
+<script setup lang="ts">
+import use_app_store from "@/store/app";
+
+const app_store = use_app_store();
+</script>
